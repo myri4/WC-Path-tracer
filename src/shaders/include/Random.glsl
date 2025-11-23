f9059fcbfe819ddce5a3c47c936fd24a7b9dc343
myri4 <perminator6644@gmail.com>
2025-11-23 16:48:34 +0200

    semi working bvh implementation

--- src/shaders/include/Random.glsl
+++ src/shaders/include/Random.glsl
@@ -15,10 +15,10 @@ uint pcg_hash(uint seed)
 }
 
 // Used to advance the PCG state.
-uint rand_pcg(inout uint rng_state)
+uint rand_pcg(inout uint rngState)
 {
-	uint state = rng_state;
-	rng_state = rng_state * 747796405u + 2891336453u;
+	uint state = rngState;
+	rngState = rngState * 747796405u + 2891336453u;
 	uint word = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
 	return (word >> 22u) ^ word;
 }
--- src/shaders/include/constants.glsl
+++ src/shaders/include/constants.glsl
@@ -1,15 +1,15 @@
 #ifndef CONSTANTS_GLSL
 #define CONSTANTS_GLSL
 
-const float Epsilon = 1e-5;
-const float bias = 1e-5f;
+const float Epsilon = 1e-4;
+const float bias = 1e-5;
 const float kInfinity = 3.402823466e+38F;
 //const float MAX_FLOAT = uintBitsToFloat(0x7f7fffffu);
 
 const float PI = 3.14159265358979323846264338327950288;
-const float TAU = PI * 2.f;
-const float HPI = PI * 0.5f;
-const float QPI = PI * 0.25f;
+const float TAU = PI * 2.0;
+const float HPI = PI * 0.5;
+const float QPI = PI * 0.25;
 const float IPI = 1.f / PI;
 const float ITAU = 1.f / TAU;
 
